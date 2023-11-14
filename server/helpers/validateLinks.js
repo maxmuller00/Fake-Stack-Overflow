dd2e89776b1fcf26ae7f8b3394fd7165c51bb133
Adisri Rajkumar <54918307+adisri03@users.noreply.github.com>
2023-11-14 11:07:27 -0500

    Delete server/helpers directory

--- server/helpers/validateLinks.js
+++
@@ -1,7 +0,0 @@
-const validateLinks = (link) => {
-  let allLinks = link.match(/\[[^\]]*\]\([^)]*\)/g) ?? [];
-    let validLinks = link.match(/\[[^\]]*\]\((https?:\/\/[^)]*)\)/g) ?? [];
-    let foundError = (allLinks.length !== validLinks.length) ? true : false;
-    return foundError;
-  } 
-export default validateLinks;
