f44875fdd78fa1836a5fa5e5fdd05ec4135222ae
Adisri Rajkumar <54918307+adisri03@users.noreply.github.com>
2023-11-28 10:01:46 -0500

    question routes

--- server/connectors/questions.js
+++ server/connectors/questions.js
@@ -223,51 +223,6 @@ async function getUnansweredQuestions() {
   }
 }
 
-//increments views of question
-router.patch('/incrementViews/:question', async (req, res) => {
-    const question = await Question.findById(req.params.question).exec();
-    if (question) {
-      question.views += 1;
-      await question.save();
-      res.send(question);
-    } else {
-      res.status(404).send('Question not found');
-    }
-  });
-
-
-  //asks new question
-  router.post('/askQuestion/', async (req, res) => {
-    let newQuestionInput = req.body;
-    const tagNames = Array.isArray(newQuestionInput.tags) ? newQuestionInput.tags : [];
-    const tags = [...new Set(tagNames)];
-    const tagIds = [];
-    for (const tag of tags) {
-      const tagExists = await Tag.findOne({ name: tag }).exec();
-      if (tagExists) {
-        tagIds.push(tagExists._id);
-      } else {
-        try {
-          const newTag = new Tag({ name: tag });
-          await newTag.save();
-          tagIds.push(newTag._id);
-        } catch (error) {
-          console.error(error);
-          res.status(500).send('Error creating tag');
-          return;
-        }
-      }
-    }
-  
-    const newQuestion = new Question({
-      title: newQuestionInput.title,
-      text: newQuestionInput.text,
-      tags: tagIds,
-      asked_by: newQuestionInput.asked_by,
-    });
-    await newQuestion.save();
-    res.send(newQuestion);
-  });
 
   //gets question
   router.get('/:question', async (req, res) => {
@@ -294,6 +249,264 @@ router.patch('/incrementViews/:question', async (req, res) => {
       res.status(500).send('Internal Server Error');
     }
   })
+
+router.get('/comments/:question_id', async (req, res) => {
+  try {
+    const question = await Questions.findById(req.params.question_id).exec();
+    const comment = await Comments.find({ _id: { $in: question.comments } }).sort({ com_date_time: -1 });
+    if (comment) {
+      res.send(comment);
+    } else {
+      res.status(404).send('Answer not found');
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+router.get('/:question', async (req, res) => {
+  try {
+    const question = await Questions.findById(req.params.question).exec();
+    if (question) {
+      res.send(question);
+    } else {
+      res.status(404).send('Question not found');
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+router.get('/byUser/:userId', async (req, res) => {
+  try {
+    // get all questions by userId then return it sorted by date
+    const questions = await Questions.find({ asked_by: req.params.userId })
+      .sort({
+        ask_date_time: -1,
+      })
+      .exec();
+    res.send(questions);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+router.patch('/incrementViews/:question', async (req, res) => {
+  const question = await Questions.findById(req.params.question).exec();
+  if (question) {
+    question.views += 1;
+    question.save();
+    res.send(question);
+  } else {
+    res.status(404).send('Question not found');
+  }
+});
+
+router.use(auth); // ANYTHING BELOW THIS WILL REQUIRE AUTHENTICATION
+
+router.post('/askQuestion', async (req, res) => {
+  let newQuestionInput = req.body;
+  newQuestionInput.askedBy = req.session.user.userId; // do not trust the client to send the user id via post request
+  const tagNames = Array.isArray(newQuestionInput.tagNames) ? newQuestionInput.tagNames : [];
+  const tags = [...new Set(tagNames)];
+  const tagIds = [];
+  for (const tag of tags) {
+    const tagExists = await Tags.findOne({ name: tag }).exec();
+    if (tagExists) {
+      tagIds.push(tagExists._id);
+    } else {
+      try {
+        const user = await Users.findOne({ _id: newQuestionInput.askedBy }).exec();
+        if (user.reputation < 50) {
+          res.send({ error: true, message: 'User must have atleast 50 reputation points to create a new tag.' });
+          return;
+        }
+        const newTag = new Tags({ name: tag, created_By: newQuestionInput.askedBy });
+        await newTag.save();
+        tagIds.push(newTag._id);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send('Error creating tag');
+        return;
+      }
+    }
+  }
+
+  try {
+    const newQuestion = new Questions({
+      title: newQuestionInput.title,
+      summary: newQuestionInput.summary,
+      text: newQuestionInput.text,
+      tags: tagIds,
+      asked_by: newQuestionInput.askedBy,
+    });
+    await newQuestion.save();
+    res.send(newQuestion);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error creating question');
+  }
+});
+
+router.put('/editQuestion/:question', async (req, res) => {
+  const question = await Questions.findById(req.params.question).exec();
+  if (question) {
+    const editQuestion = req.body;
+    question.title = editQuestion.title;
+    question.summary = editQuestion.summary;
+    question.text = editQuestion.text;
+    const tagNames = Array.isArray(editQuestion.tagNames) ? editQuestion.tagNames : [];
+    const tags = [...new Set(tagNames)];
+    const tagIds = [];
+    for (const tag of tags) {
+      const tagExists = await Tags.findOne({ name: tag }).exec();
+      if (tagExists) {
+        tagIds.push(tagExists._id);
+      } else {
+        try {
+          const user = await Users.findOne({ _id: question.asked_by }).exec();
+          if (user.reputation < 50) {
+            res.send({ error: true, message: 'User must have atleast 50 reputation points to create a new tag.' });
+            return;
+          }
+          const newTag = new Tags({ name: tag, created_By: question.asked_by });
+          await newTag.save();
+          tagIds.push(newTag._id);
+        } catch (error) {
+          console.error(error);
+          res.status(500).send('Error creating tag');
+          return;
+        }
+      }
+    }
+    question.tags = tagIds;
+    await question.save();
+    res.send(question);
+  } else {
+    res.status(404).send('Question not found');
+  }
+});
+
+router.delete('/deleteQuestion/:question', async (req, res) => {
+  try {
+    const question = await Questions.findById(req.params.question).exec();
+    if (!question) {
+      res.status(404).send('Question not found');
+      return;
+    }
+
+    question.answers.forEach(async (answer) => {
+      await Answers.findByIdAndDelete(answer).exec();
+    });
+
+    question.comments.forEach(async (comment) => {
+      await Comments.findByIdAndDelete(comment).exec();
+    });
+
+    await Questions.findByIdAndDelete(req.params.question).exec();
+    res.send('success');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+router.patch('/incrementVotes/:question/:userVoted', async (req, res) => {
+  const question = await Questions.findById(req.params.question).exec();
+  let updateUserReputation = 0;
+  if (question) {
+    let voterObj = question.voters.filter((voter) => voter.userVoted.toString() === req.params.userVoted);
+    if (voterObj.length > 0) {
+      let userVoted = voterObj[0].userVoted;
+      let currentDirection = voterObj[0].direction;
+      if (currentDirection === -1) {
+        question.votes += 1;
+        updateUserReputation = 10;
+      } else if (currentDirection === 0) {
+        question.votes += 1;
+        updateUserReputation = 5;
+      }
+      let direction = Math.min(currentDirection + 1, 1);
+      const objIndex = question.voters.findIndex((obj) => obj.userVoted == userVoted);
+      question.voters[objIndex].direction = direction;
+    } else {
+      question.votes += 1;
+      updateUserReputation = 5;
+      question.voters.push({
+        userVoted: req.params.userVoted,
+        direction: 1,
+      });
+    }
+    await question.save();
+    let userToUpdate = await Users.findOne({ _id: question.asked_by }).exec();
+    userToUpdate.reputation += updateUserReputation;
+    await userToUpdate.save();
+    res.status(200).send(question);
+  } else {
+    res.status(404).send('Question not found');
+  }
+});
+
+router.patch('/comments/incrementVotes/:comment/:userVoted', async (req, res) => {
+  const comment = await Comments.findById(req.params.comment).exec();
+  let updateUserReputation = 0;
+  if (comment) {
+    let voterObj = comment.voters.filter((voter) => voter.userVoted.toString() === req.params.userVoted);
+    if (voterObj.length === 0) {
+      comment.votes += 1;
+      updateUserReputation = 5;
+      comment.voters.push({
+        userVoted: req.params.userVoted,
+      });
+    }
+    await comment.save();
+    let userToUpdate = await Users.findOne({ _id: comment.com_by }).exec();
+    userToUpdate.reputation += updateUserReputation;
+    await userToUpdate.save();
+    res.status(200).send(comment);
+  } else {
+    res.status(404).send('Question not found');
+  }
+});
+
+router.patch('/decrementVotes/:question/:userVoted', async (req, res) => {
+  const question = await Questions.findById(req.params.question).exec();
+  let updateUserReputation = 0;
+  if (question) {
+    let voterObj = question.voters.filter((voter) => voter.userVoted.toString() === req.params.userVoted);
+    if (voterObj.length > 0) {
+      let userVoted = voterObj[0].userVoted;
+      let currentDirection = voterObj[0].direction;
+      if (currentDirection === 1) {
+        question.votes -= 1;
+        updateUserReputation = 5;
+      } else if (currentDirection === 0) {
+        question.votes -= 1;
+        updateUserReputation = 10;
+      }
+      let direction = Math.max(currentDirection - 1, -1);
+      const objIndex = question.voters.findIndex((obj) => obj.userVoted == userVoted);
+      question.voters[objIndex].direction = direction;
+    } else {
+      question.votes -= 1;
+      updateUserReputation = 10;
+      question.voters.push({
+        userVoted: req.params.userVoted,
+        direction: -1,
+      });
+    }
+    await question.save();
+    let userToUpdate = await Users.findOne({ _id: question.asked_by }).exec();
+    userToUpdate.reputation -= updateUserReputation;
+    await userToUpdate.save();
+    res.status(200).send(question);
+  } else {
+    res.status(404).send('Question not found');
+  }
+});
 
   module.exports = router;
 
